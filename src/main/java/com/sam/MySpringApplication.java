6054b8ffac27720fd1490bd044aee6264923ba18
sam <sam_shao@jiliguala.com>
2021-07-09 13:19:43 +0800

    dev

--- src/main/java/com/sam/MySpringApplication.java
+++ src/main/java/com/sam/MySpringApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("/com.sam.mapper")
 public class MySpringApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/sam/pojo/student.java
@@ -0,0 +1,6 @@
+package com.sam.pojo;
+
+public class student {
+    private int id;
+    private String name;
+}
